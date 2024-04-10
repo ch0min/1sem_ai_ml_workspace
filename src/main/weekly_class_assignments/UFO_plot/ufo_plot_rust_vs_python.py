af32e9952f93272e8b4236ac6b28eb39521f8703
Ch0min <marklundgaard7@gmail.com>
2024-04-10 13:28:29 +0200

    rust vs python plot

--- src/main/weekly_class_assignments/UFO_plot/ufo_plot_rust_vs_python.py
+++ src/main/weekly_class_assignments/UFO_plot/ufo_plot_rust_vs_python.py
@@ -2,46 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-import os
-
-# Set OMP_NUM_THREADS to 8
-os.environ["OMP_NUM_THREADS"] = "8"
-
-# Python >3.5 is required:
-import sys
-assert sys.version_info >= (3, 5)
-
-# Sciki-Learn >0.20 is required:
-import sklearn
-assert sklearn.__version__ >= "0.20"
-
-# Common imports:
-import numpy as np
-
-# To make this notebook's output stable across runs:
-np.random.seed(42)
-
-# To plot pretty figures:
-%matplotlib inline
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-mpl.rc("axes", labelsize=14)
-mpl.rc("xtick", labelsize=12)
-mpl.rc("ytick", labelsize=12)
-
-# Where to save the figures:
-PROJECT_ROOT_DIR = "."
-CHAPTER_ID = "unsupervised_learning"
-IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
-os.makedirs(IMAGES_PATH, exist_ok=True)
-
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
-    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format=fig_extension, dpi=resolution)
-
 # Data
 data = {
     'N': [15, 20, 25, 30, 35, 40],
@@ -64,6 +24,5 @@ plt.xlabel('N Value')
 plt.ylabel('Execution Time (seconds)')
 plt.yscale('log')  # Using a logarithmic scale for better visualization
 plt.grid(True, which="both", ls="--", linewidth=0.5)
-save_fig("rust_vs_python_plot")
 
 plt.show()
